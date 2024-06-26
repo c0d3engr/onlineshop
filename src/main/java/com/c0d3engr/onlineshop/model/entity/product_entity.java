61b483911e68188569ade0dae1df6651059e8401
c0d3engr <c0d3engr@gmail.com>
2024-06-27 01:20:30 +0800

    Product and Product_entity classes updated

--- src/main/java/com/c0d3engr/onlineshop/model/entity/product_entity.java
+++ src/main/java/com/c0d3engr/onlineshop/model/entity/product_entity.java
@@ -1,5 +1,39 @@
 package com.c0d3engr.onlineshop.model.entity;
 
+import java.security.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data 
+@Entity (name = "product")
 public class Product_entity {
     
+    @Id // I don't fully understand this part yet.
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private String name;
+    private String desc;
+    private Integer discount_percent;
+    private Boolean active;
+    private Timestamp created_at;
+    private Timestamp modified_at;
+    private Timestamp deleted_at;
+
+    @ManyToOne(fetch = FetchType.LAZY) // I don't fully understand this part yet.
+    @JoinColumn(name="category_id") // I don't fully understand this part yet.
+    private Product_category_entity product_category_entitycategory;
 }
--- src/main/java/com/c0d3engr/onlineshop/model/rest/product.java
+++ src/main/java/com/c0d3engr/onlineshop/model/rest/product.java
@@ -1,5 +1,25 @@
 package com.c0d3engr.onlineshop.model.rest;
 
+import java.security.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Product {
     
+    private Integer id;
+    private String name;
+    private String desc;
+    private Integer discount_percent;
+    private Boolean active;
+    private Timestamp created_at;
+    private Timestamp modified_at;
+    private Timestamp deleted_at;
+
 }
