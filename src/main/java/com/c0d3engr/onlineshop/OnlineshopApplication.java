ddd593aedcc85fc08e5668c9dbec9e0379e69a26
c0d3engr <c0d3engr@gmail.com>
2024-06-26 20:47:28 +0800

    Initial commit

---
+++ src/main/java/com/c0d3engr/onlineshop/OnlineshopApplication.java
@@ -0,0 +1,13 @@
+package com.c0d3engr.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c0d3engr/onlineshop/OnlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c0d3engr.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
