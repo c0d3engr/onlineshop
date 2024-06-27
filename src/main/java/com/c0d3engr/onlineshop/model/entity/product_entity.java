e7c6cff371fd319f2906226aa03d5d489becd7e8
c0d3engr <c0d3engr@gmail.com>
2024-06-27 12:00:24 +0800

    Unfinished code for product_entity

--- src/main/java/com/c0d3engr/onlineshop/model/entity/product_entity.java
+++ src/main/java/com/c0d3engr/onlineshop/model/entity/product_entity.java
@@ -37,3 +37,102 @@ public class Product_entity {
     @JoinColumn(name="category_id") // I don't fully understand this part yet.
     private User_entity user_entity; // This needs correction. 
 }
+
+
+
+/* 
+@Entity(name="product")
+public class Product_entity {
+	@Id
+	@Column(name = "product_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	private String image;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "category_id",referencedColumnName = "category_id")
+	private Category category;
+	
+	private int quantity;
+	
+	private int price;
+	
+	private int weight;
+	
+	private String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@ManyToOne
+    @JoinColumn(name = "customer_id")
+    private User customer;
+	
+	
+}
+    */
